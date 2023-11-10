e673d6ea13f86b0a1a18ed34dcd356a2a2b22268
kraii <matthewkerry@gmail.com>
2023-11-10 18:02:47 +0000

    Parse remaining attributes

--- parser.ts
+++ parser.ts
@@ -1,10 +1,9 @@
 import Player from "./player.ts";
 import {file} from "bun";
-import { JSDOM } from 'jsdom'
+import {JSDOM} from 'jsdom'
 import {partialRight} from 'ramda';
 
 export default async function parse(filename: string): Promise<Player[]> {
-
     const contents = await file(filename).text()
 
     const dom = new JSDOM(contents)
@@ -12,26 +11,22 @@ export default async function parse(filename: string): Promise<Player[]> {
 
     const headerKeys: Record<string, number> = {}
 
-    for(let i = 0; i < header.cells.length; i++) {
+    for (let i = 0; i < header.cells.length; i++) {
         headerKeys[header.cells[i].textContent!!] = i
     }
 
     function getValue(column: string, row: HTMLTableRowElement): string {
-        return row.cells[headerKeys[column]].textContent!!
+        return row.cells[headerKeys[column]]?.textContent || '-1'
     }
 
     function getValueN(column: string, row: HTMLTableRowElement): number {
         return Number(getValue(column, row))
     }
 
-    const players: Partial<Player>[] = []
-
-    for(let i = 0; i < rows.length; i++) {
-        const v = partialRight(getValue, [rows[i]])
-        const n = partialRight(getValueN, [rows[i]])
-        players.push({
+    function buildPlayer(v: (x1: string) => string, n: (x1: string) => number) {
+        return {
             name: v('Name'),
-            position: v('Name'),
+            position: v('Position'),
             personality: v('Personality'),
             mediaHandling: v('Media Handling'),
             leftFoot: v('Left Foot'),
@@ -51,13 +46,54 @@ export default async function parse(filename: string): Promise<Player[]> {
                 tackling: n('Tck'),
                 technique: n('Tec'),
                 throwIns: n('L Th')
-
-
             },
-            // mental: {},
-            // physical: {}
-            // goalKeeping: {}
-        })
+            mental: {
+                aggression: n('Agg'),
+                anticipation: n('Ant'),
+                bravery: n('Bra'),
+                composure: n('Com'),
+                concentration: n('Cnt'),
+                decisions: n('Dec'),
+                flair: n('Fla'),
+                determination: n('Det'),
+                leadership: n('Ldr'),
+                offTheBall: n('OtB'),
+                positioning: n('Pos'),
+                teamwork: n('Tea'),
+                vision: n('Vis'),
+                workRate: n('Wor')
+            },
+            physical: {
+                acceleration: n('Acc'),
+                agility: n('Agi'),
+                balance: n('Bal'),
+                jumping: n('Jum'),
+                natFit: n('Nat'),
+                pace: n('Pac'),
+                stamina: n('Sta'),
+                strength: n("Str")
+            },
+            goalKeeping: {
+                aerialReach: n('Aer'),
+                commandOfArea: n('Cmd'),
+                communication: n('Com'),
+                eccentricity: n('Ecc'),
+                handling: n('Han'),
+                kicking: n('Kic'),
+                oneOnOnes: n('1v1'),
+                punching: n('Pun'),
+                rushingOut: n('TRO'),
+                throwing: n('Thr')
+            }
+        };
     }
-    return players as Player[]
+
+    const players: Player[] = []
+
+    for (let i = 0; i < rows.length; i++) {
+        const v = partialRight(getValue, [rows[i]])
+        const n = partialRight(getValueN, [rows[i]])
+        players.push(buildPlayer(v, n))
+    }
+    return players
 }
